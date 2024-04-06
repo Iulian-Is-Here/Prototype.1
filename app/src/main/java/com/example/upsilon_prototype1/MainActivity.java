ac877c990ca7839471541d628d8db8e84d03b6a3
Iulian Irina S <mariairinas@gmail.com>
2024-04-06 14:37:18 +0300

    I finally figured how to proprely use this and create activities, yay

--- app/src/main/java/com/example/upsilon_prototype1/MainActivity.java
+++ app/src/main/java/com/example/upsilon_prototype1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.upsilon_prototype1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
         });
     }
 }
